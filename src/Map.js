bb98bb7eb5786acdf66552d7cd87a537aedbe4bc
geforce6t <shashwat.live@outlook.com>
2021-10-24 17:12:29 +0530

    batman commit

---
+++ src/Map.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import MapGL, { SVGOverlay } from 'react-map-gl';
+import { DeckGL, ScatterplotLayer, GeoJsonLayer, ArcLayer } from 'deck.gl';
+import { Spring } from 'react-spring/renderprops';
+import Goo from './goodies/Goo';
+import { easeBackOut, pairs, shuffle } from 'd3';
+import { lineString } from '@turf/helpers';
+
+import ArcBrushingLayer from './goodies/ArcBrushingLayer';
+
+function SvgOverlayExample({ libraries, radius }) {
+  const redraw = ({ project }) => {
+    return (
+      <g>
+        <Goo>
+          {libraries.map(lib => {
+            const [x, y] = project(lib.position);
+            return (
+              <circle key={lib.id} cx={x} cy={y} r={radius} fill="tomato" />
+            );
+          })}
+        </Goo>
+      </g>
+    );
+  };
+  return <SVGOverlay redraw={redraw} />;
+}
+
+export default function Map({
+  width,
+  height,
+  viewState,
+  onViewStateChange,
+  libraries,
+  radius,
+  arcsEnabled,
+}) {
+  const librariesMass = React.useMemo(
+    () => libraries.filter(d => d.state === 'MA'),
+    [libraries]
+  );
+
+  const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiZ2Vmb3JjZTZ0IiwiYSI6ImNrdjR4bG10MzB3d2gydm8wNmptN3VmOTQifQ.qRk-Xm4Q8KfMqKJwt5cyGA";
+
+  const librariesLine = React.useMemo(
+    () =>
+      libraries.length ? lineString(libraries.map(d => d.position)) : undefined,
+    [libraries]
+  );
+
+  const libraryLinks = React.useMemo(() => {
+    return pairs(shuffle(libraries.slice()).slice(0, 100));
+  }, [libraries]);
+
+  return (
+    <MapGL
+      width={width}
+      height={height}
+      viewState={viewState}
+      mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+      onViewStateChange={onViewStateChange}
+    >
+      <Spring to={{ arcCoef: arcsEnabled ? 1 : 1e-10 }}>
+        {springProps => {
+          const layers = [
+            new ScatterplotLayer({
+              id: 'scatterplot-layer',
+              data: libraries,
+              getRadius: 500 * radius,
+              radiusMaxPixels: 5,
+              getFillColor: [255, 99, 7],
+              transitions: {
+                getRadius: {
+                  duration: 2000,
+                  easing: easeBackOut,
+                },
+              },
+              pickable: true,
+              onClick: info => console.log(info.object),
+              autoHighlight: true,
+            }),
+            new GeoJsonLayer({
+              id: 'geojson-layer',
+              data: librariesLine,
+              lineWidthMinPixels: 1,
+              getLineColor: [0, 0, 0, 20],
+            }),
+
+            new ArcBrushingLayer({
+              id: 'arc-layer',
+              data: libraryLinks,
+              getSourcePosition: d => d[0].position,
+              getTargetPosition: d => d[1].position,
+              getSourceColor: [0, 255, 0],
+              getTargetColor: [0, 200, 2],
+              getWidth: 3,
+              visible: springProps.arcCoef > 1e-6,
+              coef: springProps.arcCoef,
+            }),
+          ];
+
+          return <DeckGL layers={layers} viewState={viewState} />;
+        }}
+      </Spring>
+      {/* <Spring from={{ radius: 0 }} to={{ radius }}>
+        {springProps => (
+          <SvgOverlayExample
+            libraries={librariesMass}
+            radius={springProps.radius}
+          />
+        )}
+        </Spring> */}
+    </MapGL>
+  );
+}
